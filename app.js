42f34d686d8e0830fc58029a57b4629d4b06d589
lowPrio12 <ninar.ranin@gmail.com>
2024-10-03 13:49:20 +0800

    added the copy of the code from a repository

---
+++ app.js
@@ -0,0 +1,10 @@
+const person1 ={
+    name:"Cliff Amadeus",
+    age:23,
+    occupation:"hqr"
+}
+
+
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
