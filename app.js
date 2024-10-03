9100497f385b4428edfbd7e57c32fd9f05398979
lowPrio12 <ninar.ranin@gmail.com>
2024-10-03 13:51:40 +0800

    changed the attributes of person 1

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const person1 ={
-    name:"Cliff Amadeus",
-    age:23,
-    occupation:"hqr"
+    name:"Jacob Israel A. Ranin Jr.",
+    age:20,
+    occupation:"Student",
+    hobby: "Read comics"
+
 }
 
 
 console.log("Name: "+person1.name);
 console.log("Age: "+person1.age);
 console.log("Occupation: "+person1.occupation);
+console.log("Hobby: "+person1.hobby);
