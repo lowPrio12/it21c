ee19ff9ee27a6ee82ad8e34cd69ba119407d80e2
lowPrio12 <ninar.ranin@gmail.com>
2024-10-07 21:18:11 +0800

    Added class, Class name Person and constructor which contains the properties of person. And console to print the output

--- Lo2-Methods.js
+++ Lo2-Methods.js
@@ -12,3 +12,21 @@ console.log("Age: " + person1.age);
 console.log("Gender: " + person1.gender);
 console.log("Hobby: " + person1.hobby);
 console.log("--------------------------------");
+
+// Class definition
+class Person {
+    constructor(name, age, gender, hobby) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.hobby = hobby;
+    }
+
+    // Method to display person information in the console
+    displayInfo() {
+        console.log("Name: " + this.name);
+        console.log("Age: " + this.age);
+        console.log("Gender: " + this.gender);
+        console.log("Hobby: " + this.hobby);
+    }
+}
