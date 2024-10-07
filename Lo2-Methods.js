394de2c92069b521ec6cac5a5a6d4b3b5f70bd40
lowPrio12 <ninar.ranin@gmail.com>
2024-10-07 21:12:36 +0800

    Added object declaration, object name and properties. And console to print the output

---
+++ Lo2-Methods.js
@@ -0,0 +1,14 @@
+//Input
+const person1 = {
+    name: "Jacob Israel A. Ranin",
+    age: 20,
+    gender: "Male",
+    hobby: "Read Comics",
+}
+
+//Process and Output
+console.log("Name: " + person1.name);
+console.log("Age: " + person1.age);
+console.log("Gender: " + person1.gender);
+console.log("Hobby: " + person1.hobby);
+console.log("--------------------------------");
