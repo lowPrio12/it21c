ae3eda5bec5d0e7545d060e4579831e3cd531f69
lowPrio12 <ninar.ranin@gmail.com>
2024-10-08 20:42:32 +0800

    Added getters and method to display on console

--- Lo4-Inheritance.js
+++ Lo4-Inheritance.js
@@ -1,13 +1,32 @@
-class Person{
+class Person {
 
     //Private Fields
     #name;
     #age;
     #occupation;
 
-    constructor(name, age, occupation){
+    constructor(name, age, occupation) {
         this.#name = name;
         this.#age = age;
         this.#occupation = occupation;
+    }
+    //Getters
+    get name() {
+        return this.#name;
+    }
+
+    get age() {
+        return this.#age;
+    }
+
+    get occupation() {
+        return this.#occupation;
+    }
+
+    // Method to display person information in the console
+    displayInfo() {
+        console.log("Name: " + this.name);
+        console.log("Age: " + this.age);
+        console.log("Occupation: " + this.occupation);
     }
 }
