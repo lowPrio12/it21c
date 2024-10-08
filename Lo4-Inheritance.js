7e6aa82aa0a129cbb098ede186f059b9ae831414
lowPrio12 <ninar.ranin@gmail.com>
2024-10-08 20:41:15 +0800

    Added Object class, constructor and parameters

---
+++ Lo4-Inheritance.js
@@ -0,0 +1,13 @@
+class Person{
+
+    //Private Fields
+    #name;
+    #age;
+    #occupation;
+
+    constructor(name, age, occupation){
+        this.#name = name;
+        this.#age = age;
+        this.#occupation = occupation;
+    }
+}
