6f911b45082d79180476b255d91b60fd46846c84
lowPrio12 <ninar.ranin@gmail.com>
2024-10-14 22:20:36 +0800

    added method to fetch data

--- charts/chart.js
+++ charts/chart.js
@@ -10,4 +10,22 @@ class ChartCreator {
             this.createCharts();
         }
     }
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            this.chartData = await response.json();
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    }
+
+    createCharts() {
+        // This method will be overridden in subclasses
+        throw new Error('createCharts() must be implemented in subclasses');
+    }
 }
+
+
